caa29d176e5221a65c2fcf92f040a81389f10bdb
Corey Hansen <chansen1532@hotmail.com>
2017-05-22 16:44:02 -0700

    Add files via upload

---
+++ src/keys.c
@@ -0,0 +1,36 @@
+
+#include "./includes/rt.h"
+
+int		key_press(int key, t_env *env)
+{
+	if (key == 13)
+		env->keys->w = 1;
+	if (key == 0)
+		env->keys->a = 1;
+	if (key == 1)
+		env->keys->s = 1;
+	if (key == 2)
+		env->keys->d = 1;
+	if (key == 12)
+		env->keys->q = 1;
+	if (key == 14)
+		env->keys->e = 1;
+	return (key);
+}
+
+int		key_release(int key, t_env *env)
+{
+	if (key == 13)
+		env->keys->w = 0;
+	if (key == 0)
+		env->keys->a = 0;
+	if (key == 1)
+		env->keys->s = 0;
+	if (key == 2)
+		env->keys->d = 0;
+	if (key == 12)
+		env->keys->q = 0;
+	if (key == 14)
+		env->keys->e = 0;
+	return (key);
+}
